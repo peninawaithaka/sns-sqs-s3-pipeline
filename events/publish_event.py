0700fc61672107c68a26ec223cad0664bb1c2438
Penina Waithaka <peninawaithaka5@gmail.com>
2025-06-18 07:43:16 +0300

    added publish event

--- events/publish_event.py
+++ events/publish_event.py
@@ -1,4 +1,24 @@
-import boto3
+import os
 import json
+import boto3
+from dotenv import load_dotenv
 
-#comming up
+load_dotenv()
+
+REGION = os.getenv("REGION")
+TOPIC_ARN = os.getenv("TOPIC_ARN")
+EVENT_FILE = 'events.json'
+
+sns = boto3.client('sns', region_name=REGION)
+
+file_path = os.path.join(os.path.dirname(__file__), EVENT_FILE)
+with open(file_path, 'r') as f:
+    event_data = json.load(f)
+
+response = sns.publish(
+    TopicArn=TOPIC_ARN,
+    Message=json.dumps(event_data),
+    Subject='User Event'
+)
+
+print(f"SNS Response", response)
